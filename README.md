124fa2a38c558200aa957c2fdc13bed0c666c3b7
engel lee <engel0088@gmail.com>
2013-05-10 21:22:46 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#In your life
+
+###Here you can find your life, how was, is, and will be going.
+
+This project is started by Engel Lee, and will be promoted as a global by its deeply focusing on life.
+
+<started in 2013, May , 9>
+##By Engel Lee
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inlife::Application.config.secret_token = 'b50d156023e9465695272be5eb5a673d31c39e60968f92b24103ffbe600d01bf7889e027db9c8c92efb34fe65384e9d2264c802d0d2c53c8061b93d2dbbf0149'
